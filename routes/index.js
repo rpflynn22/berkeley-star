c198e9da8a2e9cc7e08e96a8b12d952afef1e739
rpflynn22 <rpflynn22@gmail.com>
2015-10-12 13:17:31 -0700

    remove mongo dependencies. i think my mongo acct stuff changed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var Audition = require('../models/audition_model');
 var auditionsUsername = process.env.AUDITIONS_USERNAME;
 var auditionsPassword = process.env.AUDITIONS_PASSWORD;
 
@@ -22,49 +21,6 @@ router.get('/staff', function(req, res) {
 /* GET audition form page. */
 router.get('/audition', function(req, res) {
 	res.render('audition');
-});
-
-/* POST to add an audition. */
-router.post('/audition', function(req, res) {
-  console.log(req.body.firstName);
-  var audition = new Audition({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    year: req.body.year,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    auditionLink: req.body.auditionLink
-  });
-  audition.save(function(err) {
-    if (err) {
-      res.status(500).end();
-      
-    }
-    res.status(200).render('audition-submission');
-  });
-});
-
-router.get('/all-auditions', function(req, res) {
-  res.render('all-auditions-authentication');
-});
-
-router.post('/auditions-authentication', function(req, res) {
-  if (req.body.username == auditionsUsername && req.body.password == auditionsPassword) {
-    Audition.find(function(err, auditions) {
-  	  if (err) {
-  	    res.status(500).end();
-  	    return console.error(err);
-      }
-      console.log
-      res.render('all-auditions', {auditions: auditions});
-    });
-  } else {
-  	res.redirect('/all-auditions');
-  }
-});
-
-router.get('/auditions-authentication', function(req, res) {
-  res.redirect('/all-auditions');
 });
 
 module.exports = router;
